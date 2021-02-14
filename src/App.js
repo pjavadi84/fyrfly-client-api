03424503a7d6d3566c80effd09bdb877306df3c2
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-02-13 19:04:38 -0800

    adding Routes and Switch for app.js to handle authentication

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+import React, { Component } from 'react';
+import axios from 'axios';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+class App extends Component {
+constructor(props){
+  super(props);
+  this.state = {
+    isLoggedIn: false,
+    user: {}
+  }
+}
+
+  render() {
+    return (
+      <div>
+         <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={}/>
+            <Route exact path='/login' component={}/>
+            <Route exact path='/signup' component={}/>
+          </Switch>
+        </BrowserRouter>
+    );
+  }
 }
 
 export default App;
