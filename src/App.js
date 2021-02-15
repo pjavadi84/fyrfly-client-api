2c774b2af670fcfbc34fadcf138b5d1e328368fb
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-02-15 15:45:00 -0800

    adding handlelogin, handlelogout, and loginstatus to connect to database and handle user authentication. added lifecycle to keep track of user status

--- src/App.js
+++ src/App.js
@@ -2,13 +2,44 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 class App extends Component {
-constructor(props){
-  super(props);
-  this.state = {
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      user: {}
+    }
+  }
+
+  componentDidMount() {
+    this.loginStatus()
+  }
+
+  handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  }
+
+  handleLogout = () => {
+    this.setState({
     isLoggedIn: false,
     user: {}
+    })
   }
-}
+
+  loginStatus = () => {
+    axios.get('http://localhost:3001/logged_in', 
+   {withCredentials: true})
+    .then(response => {
+      if (response.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  }
 
   render() {
     return (
@@ -20,8 +51,10 @@ constructor(props){
             <Route exact path='/signup' component={}/>
           </Switch>
         </BrowserRouter>
+      </div>
     );
   }
+
 }
 
 export default App;
